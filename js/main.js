625f81dfd36f668ec28e13ea21bfb669b9ea7895
kimkwatson <kimwatson731@gmail.com>
2026-02-25 16:17:29 -0700

    added responsiveness

--- js/main.js
+++ js/main.js
@@ -1,11 +1,34 @@
 import { initMap as buildMap } from "./map.js";
 
 window.initMap = async () => {
-    const response = await fetch("./data/parks.json");
-    const data = await response.json();
+    const response = await fetch("https://developer.nps.gov/api/v1/parks?limit=500&api_key=amsIZUIObPexCdxaNPdbpUOq0lKUxhjwD91MOVj8");
+    const json = await response.json();
+    const parks = json.data;
+    console.log("NPS returned:", json.data.length, "of total:", json.total, "limit:", json.limit, "start:", json.start);
 
-    const parks = data.parks;
-    const types = data.types;
-
-    buildMap(parks, types);
+    buildMap(parks);
 };
+
+// Hamburger Menu
+
+const hamburger = document.querySelector('#hamButton');
+const navigation = document.querySelector('#animated');
+
+hamburger.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamburger.classList.toggle('open');
+});
+
+// Menu Selection
+
+const menuItems = document.querySelectorAll('ul.menu li');
+
+menuItems.forEach(item => {
+  item.addEventListener('click', () => {
+    // remove active from all
+    menuItems.forEach(i => i.classList.remove('active'));
+
+    // add active to the selected menu item
+    item.classList.add('active');
+  });
+});
--- js/map.js
+++ js/map.js
@@ -1,7 +1,11 @@
 export async function initMap(parks, types) {
+    const isMobile = window.innerWidth <= 860;
+    const zoomLevel = isMobile ? 3.2 : 5;
+    const pinScale = isMobile ? .4 : 1;
+    
     const map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: 39.8293, lng: -98.5795 },
-        zoom: 5,
+        center: { lat: 36.8293, lng: -95.5795 },
+        zoom: zoomLevel,
         scrollwheel: false,
         mapTypeControl: false,
         mapId: "59465a2252ededec98830f0d",
@@ -10,25 +14,42 @@ export async function initMap(parks, types) {
     const { AdvancedMarkerElement, PinElement } =
     await google.maps.importLibrary("marker");
 
+    types = {
+    "National Park": { "background": "#fe5f55", "accent": "#ff9a93" },
+    "National Monument": { "background": "#70ae6e", "accent": "#a6d3a4" },
+    "National Recreation Area": { "background": "#fabc3c", "accent": "#ffe29a" },
+    "National Seashore": { "background": "#4ea5d9", "accent": "#8fc9ea" },
+    "National Lakeshore": { "background": "#4ea5d9", "accent": "#8fc9ea" },
+    "National Historic Site": { "background": "#ef8a17", "accent": "#f4b46a" },
+    "National Preserve": { "background": "#add9f4", "accent": "#d8ecfa" },
+    "National Memorial": { "background": "#df7373", "accent": "#f0aaaa" },
+    default: { "background": "#7776bc", "accent": "#b1b0e0" },
+    }
+
     parks.forEach(park => {
 
         const infowindow = new google.maps.InfoWindow({
             content: buildPopup(park),
-            ariaLabel: park.name,
+            ariaLabel: park.fullName,
         })
-        const position = { lat: park.lat, lng: park.lng };
+        const lat = Number(park.latitude);
+        const lng = Number(park.longitude);
+        const position = { lat: lat, lng: lng };
         const marker = new AdvancedMarkerElement({
         map,
         position,
-        title: park.name,
+        title: park.fullName,
         });
 
         // create pin element
+        const typeKey = park.designation ?? "default";
+        const style = types[typeKey] ?? types.default;
+
         const pin = new PinElement({
-            scale: 1,
-            background: types[park.type].background,
-            borderColor: types[park.type].accent,
-            glyphColor: types[park.type].accent,
+            scale: pinScale,
+            background: style.background,
+            borderColor: style.accent,
+            glyphColor: style.accent,
         });
         marker.content = pin;
     
@@ -45,9 +66,12 @@ export async function initMap(parks, types) {
 function buildPopup(park) {
     return `
         <div id="popup">
-            <h3>${park.name}</h3>
-            <p>${park.state}</p>
-            <p>${park.type}</p>
+            <h3 id="popup-name">${park.fullName}</h3>
+            ${park.images?.[0]?.url ? `<img id="popup-image" src="${park.images[0].url}" alt="${park.fullName}"/>` : ""}
+            <p id="popup-description">${park.description}</p>
+            <p id="popup-states"><strong>States:</strong> ${park.states}</p>
+            <a id="popup-link" href="${park.url}" target="_blank">Official site</a>
+            
         </div>
     `
 }
